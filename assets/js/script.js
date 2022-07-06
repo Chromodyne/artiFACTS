8517ccccbc82a1629c642e24a1eea8be6a0e5413
chromodyne <dsinitiere@outlook.com>
2022-07-06 08:59:13 -0700

    Fixed API call.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,20 +13,26 @@ const options = {
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
 
-//Google News API Settings
-const options2 = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'PLACEHOLDER',
-		'X-RapidAPI-Host': 'google-news1.p.rapidapi.com'
-	}
-};
+callNewsApi();
 
 //Google News API Call
-// fetch('https://google-news1.p.rapidapi.com/list-languages?country=%3CREQUIRED%3E', options2)
-// 	.then(response => response.json())
-// 	.then(response => readyNewsData(response))
-// 	.catch(err => console.error(err));
+function callNewsApi() {
+
+    const options2 = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6ec6cf3aa6msh04ad88a208dda4ap1e0104jsn10035a179610',
+            'X-RapidAPI-Host': 'google-news1.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://google-news1.p.rapidapi.com/top-headlines?country=US&lang=en&limit=25&media=true', options2).then(
+        (resp) => {return resp.json();}).then (
+        (data) => {  readyNewsData(data);
+            console.log(data);
+    });
+
+}
 
 //Takes in the response from the Google News API
 function readyNewsData(newsData) {
@@ -40,5 +46,8 @@ function readyNewsData(newsData) {
     //news-description
     document.getElementById("news-description").textContent = localNewsData.articles[0].description;
 
+}
+
+function nextArticles() {
 
 }
